302ddb066100b36db0a0dcf5a4a6b2b75e9923ad
Kenny Belitzky <kennybz@gmail.com>
2009-03-08 19:13:37 -0300

    created egg and moved directories to packages

---
+++ deam/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.4.3'
+
+setup(name='deam',
+      version=version,
+      description="django external apps manager",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='django apps custom-command',
+      author='Kenny Belizky & Martin Saizar',
+      author_email='kenny@belitzky.com',
+      url='',
+      license='',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
