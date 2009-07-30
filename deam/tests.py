8644d2d073c4d74676e9bcb18159375d274c71ed
Kenny Belitzky <kenny@belitzky.com>
2009-07-30 15:11:30 -0300

    added tests skeleton

---
+++ deam/tests.py
@@ -0,0 +1,16 @@
+'''
+Created on Jul 2, 2009
+
+@author: Kenneth Belitzky
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+    """
+        Testing module
+    """
+
+    def setUp(self):
+        """ method for object initialization """
+        return unittest.TestCase.setUp(self)
